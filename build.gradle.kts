7f8c2fec74fbedb1eeef0d1df84ff1bc54abc52d
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-03-21 19:05:25 +0100

    Add default build tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
+defaultTasks("build", "shadowJar")
+
 allprojects {
     group = "de.natrox"
     version = "1.0.1-SNAPSHOT"
