4a1457df9fb1e89b1849b992cde33def21ee8cb5
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-01 18:49:21 +0200

    Update TaskBatchTest

--- src/test/java/TaskBatchTest.java
+++ src/test/java/TaskBatchTest.java
@@ -4,19 +4,23 @@ import java.util.concurrent.TimeUnit;
 
 public class TaskBatchTest {
 
+    public static void main(String[] args) {
+        new TaskBatchTest().test();
+    }
+
     public void test() {
         var factory = new SimpleTaskBatchFactory();
 
         factory.createTaskBatch()
             .sync(() -> {
-
+                System.out.println("1");
             })
             .async(() -> {
-
+                System.out.println("2");
             })
             .wait(10, TimeUnit.SECONDS)
             .sync(() -> {
-
+                System.out.println("3");
             })
             .execute();
 
