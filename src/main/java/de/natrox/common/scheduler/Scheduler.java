048431a328b50f7c487272384eaa289fbae04f04
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-07 22:50:36 +0200

    java doc Scheduler

--- src/main/java/de/natrox/common/scheduler/Scheduler.java
+++ src/main/java/de/natrox/common/scheduler/Scheduler.java
@@ -2,6 +2,9 @@ package de.natrox.common.scheduler;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a scheduler to execute tasks.
+ */
 public interface Scheduler {
 
     @NotNull
@@ -9,9 +12,20 @@ public interface Scheduler {
         return new SchedulerImpl();
     }
 
-    @NotNull
-    Task.Builder buildTask(@NotNull Runnable task);
+    /**
+     * Initializes a new {@link Task.Builder} for creating a task on the proxy.
+     *
+     * @param task the task to run when scheduled
+     * @return the task builder
+     */
+    @NotNull Task.Builder buildTask(@NotNull Runnable task);
 
+    /**
+     * Shutdowns the scheduler and cancel all running tasks.
+     *
+     * @return true if this executor terminated and false if 10 seconds elapsed before termination
+     * @throws InterruptedException if interrupted while waiting
+     */
     boolean shutdown() throws InterruptedException;
 
 }
--- src/main/java/de/natrox/common/scheduler/Task.java
+++ src/main/java/de/natrox/common/scheduler/Task.java
@@ -5,42 +5,98 @@ import org.jetbrains.annotations.NotNull;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Represents a task that is scheduled to run.
+ */
 public interface Task {
 
-    @NotNull
-    Scheduler owner();
+    /**
+     * Returns the scheduler on which the task is running.
+     *
+     * @return the scheduler on which the task is running.
+     */
+    @NotNull Scheduler owner();
 
-    @NotNull
-    TaskStatus status();
+    /**
+     * Returns the current status of this task.
+     *
+     * @return the current status of this task
+     */
+    @NotNull TaskStatus status();
 
+    /**
+     * Cancels this task. If the task is already running, the thread in which it is running will be
+     * interrupted. If the task is not currently running, the scheduler will terminate it safely.
+     */
     void cancel();
 
+    /**
+     * Represents a fluent interface to schedule tasks.
+     */
     interface Builder {
 
-        @NotNull
-        Builder delay(long time, TimeUnit unit);
+        /**
+         * Specifies that the task should delay its execution by the specified amount of time.
+         *
+         * @param time the time to delay by
+         * @param unit the unit of time for {@code time}
+         * @return this builder, for chaining
+         */
+        @NotNull Builder delay(long time, TimeUnit unit);
 
+        /**
+         * Specifies that the task should delay its execution by the specified amount of time.
+         *
+         * @param duration the duration of the delay
+         * @return this builder, for chaining
+         */
         @NotNull
         default Builder delay(Duration duration) {
             return delay(duration.toMillis(), TimeUnit.MILLISECONDS);
         }
 
-        @NotNull
-        Builder repeat(long time, TimeUnit unit);
+        /**
+         * Specifies that the task should continue running after waiting for the specified amount, until
+         * it is cancelled.
+         *
+         * @param time the time to delay by
+         * @param unit the unit of time for {@code time}
+         * @return this builder, for chaining
+         */
+        @NotNull Builder repeat(long time, TimeUnit unit);
 
+        /**
+         * Specifies that the task should continue running after waiting for the specified amount, until
+         * it is cancelled.
+         *
+         * @param duration the duration of the delay
+         * @return this builder, for chaining
+         */
         @NotNull
         default Builder repeat(Duration duration) {
             return repeat(duration.toMillis(), TimeUnit.MILLISECONDS);
         }
 
-        @NotNull
-        Builder clearDelay();
+        /**
+         * Clears the delay on this task.
+         *
+         * @return this builder, for chaining
+         */
+        @NotNull Builder clearDelay();
 
-        @NotNull
-        Builder clearRepeat();
+        /**
+         * Clears the repeat interval on this task.
+         *
+         * @return this builder, for chaining
+         */
+        @NotNull Builder clearRepeat();
 
-        @NotNull
-        Task schedule();
+        /**
+         * Schedules this task for execution.
+         *
+         * @return the scheduled task
+         */
+        @NotNull Task schedule();
 
     }
 
