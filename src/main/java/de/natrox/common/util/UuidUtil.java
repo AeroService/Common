81689591d9083ceb840bb62aa0168c883098b7d3
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-08 19:20:32 +0200

    [skip ci] java doc UuidUtil

--- src/main/java/de/natrox/common/util/UuidUtil.java
+++ src/main/java/de/natrox/common/util/UuidUtil.java
@@ -5,12 +5,21 @@ import org.jetbrains.annotations.NotNull;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
+/**
+ * Holds some utility methods to work with uuids.
+ */
 public final class UuidUtil {
 
     private UuidUtil() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Returns the {@link UUID} of a string.
+     *
+     * @param name the string for the generation of the {@link UUID}
+     * @return the {@link UUID} of the specified string
+     */
     public static @NotNull UUID fromName(@NotNull String name) {
         return UUID.nameUUIDFromBytes(name.toLowerCase().getBytes(StandardCharsets.UTF_8));
     }
---
+++ src/test/java/UuidTest.java
@@ -0,0 +1,11 @@
+import de.natrox.common.util.UuidUtil;
+
+public class UuidTest {
+
+    public static void main(String[] args) {
+        var uuid = UuidUtil.fromName("Test");
+
+
+    }
+
+}
