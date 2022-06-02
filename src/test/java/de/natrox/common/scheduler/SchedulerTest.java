eee66f408582ed836edbdba776255e924631b022
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-06-02 18:58:27 +0200

    Add SchedulerTest

--- src/test/java/de/natrox/common/scheduler/SchedulerTest.java
+++ src/test/java/de/natrox/common/scheduler/SchedulerTest.java
@@ -18,11 +18,48 @@ package de.natrox.common.scheduler;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SchedulerTest {
 
     @Test
-    public void test() {
-        //TODO:
+    void buildTaskTest() throws Exception {
+        Scheduler scheduler = Scheduler.create();
+        CountDownLatch latch = new CountDownLatch(1);
+        Task task = scheduler
+            .buildTask(latch::countDown)
+            .schedule();
+        latch.await();
+        assertEquals(TaskStatus.FINISHED, task.status());
+    }
+
+    @Test
+    void cancelTest() throws Exception {
+        Scheduler scheduler = Scheduler.create();
+        AtomicInteger i = new AtomicInteger(3);
+        Task task = scheduler.buildTask(i::decrementAndGet)
+            .delay(100, TimeUnit.SECONDS)
+            .schedule();
+        task.cancel();
+        Thread.sleep(200);
+        assertEquals(3, i.get());
+        assertEquals(TaskStatus.CANCELLED, task.status());
+    }
+
+    @Test
+    void repeatTaskTest() throws Exception {
+        Scheduler scheduler = Scheduler.create();
+        CountDownLatch latch = new CountDownLatch(3);
+        Task task = scheduler.buildTask(latch::countDown)
+            .delay(100, TimeUnit.MILLISECONDS)
+            .repeat(100, TimeUnit.MILLISECONDS)
+            .schedule();
+        latch.await();
+        task.cancel();
     }
 
 }
