117daee9ed8bff08b9ca955a147ee71e2160a858
gabl22 <gabl22ytmc@gmail.com>
2022-06-03 21:52:17 +0200

    Added JavaDocs

--- src/main/java/de/natrox/common/counter/CounterStatus.java
+++ src/main/java/de/natrox/common/counter/CounterStatus.java
@@ -2,7 +2,18 @@ package de.natrox.common.counter;
 
 public enum CounterStatus {
 
+    /**
+     * The {@link Counter} is currently not running, it can be started at any given time.
+     */
     IDLING,
+    /**
+     * The {@link Counter} is currently running and performing actions.
+     * It can be interrupted or paused at any time.
+     */
     RUNNING,
+    /**
+     * The {@link Counter} is currently paused, it is not performing any actions.
+     * It can be resumed at any time.
+     */
     PAUSED
 }
