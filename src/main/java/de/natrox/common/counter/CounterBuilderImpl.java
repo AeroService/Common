7b7cfaf41e5efc4f6474d34ecd7569e823372bde
gabl22 <gabl22ytmc@gmail.com>
2022-06-03 14:58:44 +0200

    Added handlers to CounterBuilderImpl

--- src/main/java/de/natrox/common/counter/CounterBuilderImpl.java
+++ src/main/java/de/natrox/common/counter/CounterBuilderImpl.java
@@ -3,8 +3,14 @@ package de.natrox.common.counter;
 import de.natrox.common.scheduler.Scheduler;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
-public final class CounterBuilderImpl implements Counter.Builder {
+final class CounterBuilderImpl implements Counter.Builder {
+
+    private Consumer<CounterInfo> startHandler;
+    private Consumer<CounterInfo> tickHandler;
+    private Consumer<CounterInfo> finishHandler;
+    private Consumer<CounterInfo> cancelHandler;
 
     private long startTime;
     private long stopTime;
@@ -12,6 +18,8 @@ public final class CounterBuilderImpl implements Counter.Builder {
     private long tick;
     private TimeUnit tickUnit;
 
+    CounterBuilderImpl() {
+    }
 
     @Override
     public Counter.Builder startTime(long startTime) {
@@ -39,12 +47,36 @@ public final class CounterBuilderImpl implements Counter.Builder {
     }
 
     @Override
+    public Counter.Builder startHandler(Consumer<CounterInfo> startHandler) {
+        this.startHandler = startHandler;
+        return this;
+    }
+
+    @Override
+    public Counter.Builder tickHandler(Consumer<CounterInfo> tickHandler) {
+        this.tickHandler = tickHandler;
+        return this;
+    }
+
+    @Override
+    public Counter.Builder finishHandler(Consumer<CounterInfo> finishHandler) {
+        this.finishHandler = finishHandler;
+        return this;
+    }
+
+    @Override
+    public Counter.Builder cancelHandler(Consumer<CounterInfo> cancelHandler) {
+        this.cancelHandler = cancelHandler;
+        return this;
+    }
+
+    @Override
     public Countdown buildCountdown() {
-        return new Countdown(scheduler, startTime, stopTime, tick, tickUnit);
+        return new Countdown(scheduler, startTime, stopTime, tick, tickUnit, startHandler, tickHandler, finishHandler, cancelHandler);
     }
 
     @Override
     public Timer buildTimer() {
-        return new Timer(scheduler, startTime, stopTime, tick, tickUnit);
+        return new Timer(scheduler, startTime, stopTime, tick, tickUnit, startHandler, tickHandler, finishHandler, cancelHandler);
     }
 }
