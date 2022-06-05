dbbf842b797f0eeb014ccef0896c45a54ae16b22
gabl22 <gabl22ytmc@gmail.com>
2022-06-05 21:21:13 +0200

    Added SupplierTest

---
+++ src/test/java/de/natrox/common/supplier/SupplierTest.java
@@ -0,0 +1,29 @@
+package de.natrox.common.supplier;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.ThrowingSupplier;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SupplierTest {
+
+    @Test
+    void throwableSupplierTest() {
+        ThrowableSupplier<String, IllegalAccessException> supplier = this::supplyString;
+        assertThrows(IllegalAccessException.class, supplier::get);
+    }
+
+    @Test
+    void catchingSupplierTest() {
+        CatchingSupplier<String> supplier = new CatchingSupplier<>(this::supplyString);
+        assertDoesNotThrow(supplier::get);
+    }
+
+    public String supplyString() {
+        throw new IllegalArgumentException();
+    }
+}
