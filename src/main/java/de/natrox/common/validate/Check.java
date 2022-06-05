e6f38888b865257e3de07f18d672d4246664ad88
gabl22 <gabl22ytmc@gmail.com>
2022-06-05 19:35:43 +0200

    Flattened methods

--- src/main/java/de/natrox/common/validate/Check.java
+++ src/main/java/de/natrox/common/validate/Check.java
@@ -34,30 +34,30 @@ public final class Check {
 
     @Contract("null, _ -> fail")
     public static void notNull(@Nullable Object object, @NotNull String reason) {
-        if (Objects.isNull(object)) {
-            throw new NullPointerException(reason);
-        }
+        if (!Objects.isNull(object))
+            return;
+        throw new NullPointerException(reason);
     }
 
     @Contract("null, _, _ -> fail")
     public static void notNull(@Nullable Object object, @NotNull String reason, Object... arguments) {
-        if (Objects.isNull(object)) {
-            throw new NullPointerException(MessageFormat.format(reason, arguments));
-        }
+        if (!Objects.isNull(object))
+            return;
+        throw new NullPointerException(MessageFormat.format(reason, arguments));
     }
 
     @Contract("true, _ -> fail")
     public static void argCondition(boolean condition, @NotNull String reason) {
-        if (condition) {
-            throw new IllegalArgumentException(reason);
-        }
+        if (!condition)
+            return;
+        throw new IllegalArgumentException(reason);
     }
 
     @Contract("true, _, _ -> fail")
     public static void argCondition(boolean condition, @NotNull String reason, Object... arguments) {
-        if (condition) {
-            throw new IllegalArgumentException(MessageFormat.format(reason, arguments));
-        }
+        if (!condition)
+            return;
+        throw new IllegalArgumentException(MessageFormat.format(reason, arguments));
     }
 
     @Contract("_ -> fail")
@@ -67,16 +67,15 @@ public final class Check {
 
     @Contract("true, _ -> fail")
     public static void stateCondition(boolean condition, @NotNull String reason) {
-        if (condition) {
-            throw new IllegalStateException(reason);
-        }
+        if (!condition)
+            return;
+        throw new IllegalStateException(reason);
     }
 
     @Contract("true, _, _ -> fail")
     public static void stateCondition(boolean condition, @NotNull String reason, Object... arguments) {
-        if (condition) {
-            throw new IllegalStateException(MessageFormat.format(reason, arguments));
-        }
+        if (!condition)
+            return;
+        throw new IllegalStateException(MessageFormat.format(reason, arguments));
     }
-
 }
