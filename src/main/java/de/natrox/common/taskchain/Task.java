e6991556da66adccfb532c14f1514b82e8eda3bc
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-06-20 01:01:44 +0200

    Add java docs

--- src/main/java/de/natrox/common/taskchain/Task.java
+++ src/main/java/de/natrox/common/taskchain/Task.java
@@ -4,11 +4,19 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Represents a task that can be executed on any thread.
+ */
 public interface Task extends Runnable {
 
     @Override
     void run();
 
+    /**
+     * Represents a task that does not return immediately. A supplied Consumer controls when
+     * the chain should proceed to the next task. This is a Callback style API
+     * in relation to the Future based API.
+     */
     interface CallbackTask extends Task {
 
         @Override
@@ -21,6 +29,9 @@ public interface Task extends Runnable {
         void run(@NotNull Runnable callback);
     }
 
+    /**
+     * Represents a task that returns a future to be completed later.
+     */
     interface FutureTask extends Task {
 
         @Override
--- src/main/java/de/natrox/common/taskchain/TaskExecutor.java
+++ src/main/java/de/natrox/common/taskchain/TaskExecutor.java
@@ -4,14 +4,39 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Represents an executor for tasks of {@link TaskChain}.
+ */
 public interface TaskExecutor {
 
+    /**
+     * Returns whether the current thread is the main thread or not.
+     *
+     * @return true, if the current thread is the main thread and false if not
+     */
     boolean isMainThread();
 
+    /**
+     * Schedule a runnable to run on the main thread.
+     *
+     * @param runnable the runnable to run
+     */
     void executeInMain(@NotNull Runnable runnable);
 
+    /**
+     * Run the runnable in a new thread.
+     *
+     * @param runnable the runnable to run
+     */
     void executeAsync(@NotNull Runnable runnable);
 
+    /**
+     * Schedule a runnable with delay.
+     *
+     * @param runnable the runnable to run
+     * @param delay the time to delay by
+     * @param timeUnit the unit of time for {@code time}
+     */
     void executeWithDelay(@NotNull Runnable runnable, long delay, @NotNull TimeUnit timeUnit);
 
 }
