885828ec71e92d827bf57ec7750f3e5bbed025b0
gabl <gabl22ytmc@gmail.com>
2022-06-27 21:59:02 +0200

    Simplified RunnableTests

--- src/test/java/de/natrox/common/runnable/CatchingRunnableTest.java
+++ src/test/java/de/natrox/common/runnable/CatchingRunnableTest.java
@@ -1,5 +1,6 @@
 package de.natrox.common.runnable;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,21 +9,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CatchingRunnableTest {
 
-    private static int a;
+    private static int a = 1;
+    private static CatchingRunnable runnable;
 
-    @BeforeEach
-    void init() {
-        a = 1;
-    }
-
-    @Test
-    void runTest1() {
-        CatchingRunnable runnable = new CatchingRunnable(() -> {
+    @BeforeAll
+    static void init() {
+        runnable = new CatchingRunnable(() -> {
             if(a % 2 == 0)
                 throw new IllegalStateException();
             a += a;
         });
+    }
+
+    @Test
+    void runTest1() {
         assertDoesNotThrow(runnable::run);
+        assertEquals(2, a);
+    }
+
+    @Test
+    void runTest2() {
         assertThrows(IllegalStateException.class, runnable::run);
         assertEquals(2, a);
     }
--- src/test/java/de/natrox/common/runnable/ThrowableRunnableTest.java
+++ src/test/java/de/natrox/common/runnable/ThrowableRunnableTest.java
@@ -1,5 +1,6 @@
 package de.natrox.common.runnable;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,33 +9,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class ThrowableRunnableTest {
 
     private static int a;
+    private static ThrowableRunnable<Exception> runnable;
 
-    @BeforeEach
-    void init() {
-        a = 1;
+    @BeforeAll
+    static void init() {
+        runnable = () -> {
+            if(a % 2 == 0)
+                throw new Exception();
+            a += a;
+        };
     }
 
     @Test
     void runTest1() {
-        ThrowableRunnable<IllegalStateException> runnable = () -> {
-            if(a % 2 == 0)
-                throw new IllegalStateException();
-            a += a;
-        };
         assertDoesNotThrow(runnable::run);
-        assertThrows(IllegalStateException.class, runnable::run);
         assertEquals(2, a);
     }
 
     @Test
     void runTest2() {
-        ThrowableRunnable<Exception> runnable = () -> {
-            if(a % 2 == 0)
-                throw new Exception();
-            a += a;
-        };
-        assertDoesNotThrow(runnable::run);
-        assertThrows(Exception.class, runnable::run);
+        assertThrows(IllegalStateException.class, runnable::run);
         assertEquals(2, a);
     }
 }
