6d5a0187d246add2af7cc2fd7f461f11e3737b1b
gabl <gabl22ytmc@gmail.com>
2022-06-27 20:30:13 +0200

    Removed Atomics

--- src/test/java/de/natrox/common/consumer/TriConsumerTest.java
+++ src/test/java/de/natrox/common/consumer/TriConsumerTest.java
@@ -1,28 +1,20 @@
 package de.natrox.common.consumer;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TriConsumerTest {
 
-    private static AtomicInteger mathResult;
+    private static int mathResult;
     private static String concatResult;
-
-    @BeforeAll
-    static void init() {
-        mathResult = new AtomicInteger();
-    }
 
     @Test
     void acceptTest1() {
         TriConsumer<Integer, Integer, Integer> consumer = this::doMath;
         consumer.accept(5, 1, 7);
-        assertEquals(28, mathResult.get());
+        assertEquals(28, mathResult);
     }
 
     @Test
@@ -39,7 +31,7 @@ class TriConsumerTest {
     }
 
     void doMath(int a, int b, int c) {
-        mathResult.set(a + 2 * b + 3 * c);
+        mathResult = a + 2 * b + 3 * c;
     }
 
     void concat(Object a, Object b, Object c) {
