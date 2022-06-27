e68a855beeb5f2c87f7a28e3b6e160320f0e7e52
gabl <gabl22ytmc@gmail.com>
2022-06-27 19:52:04 +0200

    Added CatchingFunctionTest

---
+++ src/test/java/de/natrox/common/function/CatchingFunctionTest.java
@@ -0,0 +1,45 @@
+package de.natrox.common.function;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CatchingFunctionTest {
+
+    @Test
+    void defaultApplyTest() {
+        {
+            CatchingFunction<String, Character> function = new CatchingFunction<>(this::firstChar);
+            assertEquals('f', function.apply("foo"));
+            assertEquals('b', function.apply("boo"));
+        }
+        {
+            CatchingFunction<Double, Double> function = new CatchingFunction<>(this::inverse);
+            assertEquals(1D/2, function.apply(2D));
+            assertEquals(2D, function.apply(1D/2));
+        }
+    }
+
+    @Test
+    void exceptionApplyTest() {
+        {
+            CatchingFunction<String, Character> function =  new CatchingFunction<>(this::firstChar);
+            assertThrows(StringIndexOutOfBoundsException.class, ()  -> function.apply(""));
+        }
+        {
+            CatchingFunction<Double, Double> function =  new CatchingFunction<>(this::inverse);
+            assertThrows(IllegalArgumentException.class, ()  -> function.apply(0D));
+        }
+    }
+
+    char firstChar(String s) {
+        return s.charAt(0);
+    }
+
+    double inverse(double a) {
+        if(a == 0)
+            throw new IllegalArgumentException();
+        return 1 / a;
+    }
+}
