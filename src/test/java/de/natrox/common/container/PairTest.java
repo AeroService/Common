055dcf99a8716831d0576a648ecba431478aa503
gabl <gabl22ytmc@gmail.com>
2022-06-28 18:00:19 +0200

    Added JUnit Assertion messages for container

--- src/test/java/de/natrox/common/container/PairTest.java
+++ src/test/java/de/natrox/common/container/PairTest.java
@@ -26,55 +26,55 @@ class PairTest {
     @Test
     void equalsTest() {
         Pair<String, String> pair = Pair.of("first", "second");
-        assertNotEquals(Pair.empty(), pair);
+        assertNotEquals(Pair.empty(), pair, "A not-empty Pair should not equal an empty Pair.");
     }
 
     @Test
     void equalsTest2() {
         Pair<?, ?> pair = Pair.empty();
-        assertEquals(Pair.empty(), pair);
+        assertEquals(Pair.empty(), pair, "An empty Pair should equal an empty Pair.");
     }
 
     @Test
     void equalsTest3() {
         Pair<String, String> pair = Pair.of("first", "second");
-        assertEquals(Pair.of("first", "second"), pair);
+        assertEquals(Pair.of("first", "second"), pair, "Two Pairs containing equal items should equal.");
     }
 
     @Test
     void equalsTest4() {
-        assertNotEquals("o", Pair.empty());
+        assertNotEquals("o", Pair.empty(), "An empty Pair should not equal a String.");
     }
 
     @Test
     void equalsTest5() {
         Pair<String, Pair<?, ?>> pair = Pair.of("first", Pair.empty());
-        assertNotEquals(Pair.of("first", "second"), pair);
+        assertNotEquals(Pair.of("first", "second"), pair, "Two Pairs should only equal if both items of each are equal.");
     }
 
     @Test
     void equalsTest6() {
         Pair<Pair<?, ?>, String> pair = Pair.of(Pair.empty(), "second");
-        assertNotEquals(Pair.empty(), pair);
+        assertNotEquals(Pair.empty(), pair, "Two Pairs should only equal if both items of each are equal.");
     }
 
     @Test
     void equalsTest7() {
         Pair<?, String> pair = Pair.of(null, "second");
-        assertNotEquals(Pair.empty(), pair);
+        assertNotEquals(Pair.empty(), pair, "Two Pairs should only equal if both items of each are equal.");
     }
 
     @Test
     void setFirstTest() {
         Pair<String, String> pair = Pair.empty();
         pair.setFirst("first");
-        assertEquals("first", pair.first());
+        assertEquals("first", pair.first(), "Two Pairs should equal also if the first item is set after initialisation.");
     }
 
     @Test
     void setSecondTest() {
         Pair<String, String> pair = Pair.empty();
         pair.setSecond("second");
-        assertEquals("second", pair.second());
+        assertEquals("second", pair.second(), "Two Pairs should equal also if the second item is set after initialisation.");
     }
 }
