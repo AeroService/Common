6e9c32898504415133bd9a4941c5fad6c80cc9c9
gabl <gabl22ytmc@gmail.com>
2022-06-29 16:58:55 +0200

    Rewrote QuadConsumerTest

--- src/test/java/de/natrox/common/consumer/QuadConsumerTest.java
+++ src/test/java/de/natrox/common/consumer/QuadConsumerTest.java
@@ -2,39 +2,31 @@ package de.natrox.common.consumer;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class QuadConsumerTest {
 
-    private static int doMathResult;
-    private static String concatResult;
-
     @Test
-    void applyTest1() {
-        QuadConsumer<Integer, Integer, Integer, Integer> consumer = this::doMath;
-        consumer.accept(5, 1, 7, 2);
-        assertEquals(38, doMathResult, "Consumer should give expected output 38 for 5 + 2*1 + 3*7 + 5*2.");
+    void defaultApplyTest() {
+        AtomicInteger indicator = new AtomicInteger();
+        QuadConsumer<Integer, Integer, Integer, Integer> consumer = (a, b, c, d) -> indicator.set(this.addition(a, b, c, d));
+        consumer.accept(1, 2, 3, 4);
+        assertEquals(10, indicator.get(), "Indicator should indicate the input sum of 10.");
+        consumer.accept(5, 4, 3, 2);
+        assertEquals(14, indicator.get(), "Indicator should indicate the input sum of 14.");
     }
 
     @Test
-    void applyTest2() {
-        QuadConsumer<Integer, Long, Boolean, Double> consumer = this::concat;
-        consumer.accept(12, 34567L, true, 8.9D);
-        assertEquals("1234567true8.9", concatResult, "Consumer should give expected output \"1234567true8.9\" as 12, 334567L, true, 8.9D get combined.");
-    }
-
-    @Test
-    void nullTest() {
-        QuadConsumer<Integer, Integer, Integer, Integer> consumer = this::doMath;
+    void nullApplyTest() {
+        QuadConsumer<Integer, Integer, Integer, Integer> consumer = this::addition;
         assertThrows(NullPointerException.class, () ->
             consumer.accept(null, null, null, null), "Consumer should throw a NullPointerException if the arguments are null.");
     }
 
-    void doMath(int a, int b, int c, int d) {
-        doMathResult = a + 2 * b + 3 * c + 5 * d;
-    }
-
-    void concat(Object a, Object b, Object c, Object d) {
-        concatResult = a.toString() + b.toString() + c.toString() + d.toString();
+    private int addition(int a, int b, int c, int d) {
+        return a + b + c + d;
     }
 }
