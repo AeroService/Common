525445dc4002ae1927f6b088bf8169beee2c5b8a
gabl <gabl22ytmc@gmail.com>
2022-06-29 16:50:38 +0200

    Rewrote CatchingConsumerTest

--- src/test/java/de/natrox/common/consumer/CatchingConsumerTest.java
+++ src/test/java/de/natrox/common/consumer/CatchingConsumerTest.java
@@ -2,52 +2,33 @@ package de.natrox.common.consumer;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CatchingConsumerTest {
 
-    private static char firstCharResult;
-    private static double invertResult;
-
     @Test
-    void defaultApplyTest1() {
-        CatchingConsumer<String> consumer = new CatchingConsumer<>(this::firstChar);
-        consumer.accept("foo");
-        assertEquals('f', firstCharResult, "Consumer should give expected output 'f' as the first char of \"foo\".");
-        consumer.accept("boo");
-        assertEquals('b', firstCharResult, "Consumer should give expected output 'b' as the first char of \"boo\".");
+    void defaultApplyTest() {
+        AtomicInteger indicator = new AtomicInteger();
+        CatchingConsumer<Integer> consumer = new CatchingConsumer<>((a) -> indicator.set(value(a)));
+        consumer.accept(1);
+        assertEquals(1, indicator.get(), "Consumer should have performed actions correctly.");
+        consumer.accept(2);
+        assertEquals(2, indicator.get(), "Consumer should have performed actions correctly.");
     }
 
     @Test
-    void defaultApplyTest2() {
-        CatchingConsumer<Double> consumer = new CatchingConsumer<>(this::inverse);
-        consumer.accept(2D);
-        assertEquals(1D / 2, invertResult, "Consumer should give expected output .5 as the multiplicative inverse of 2.");
-        consumer.accept(1D / 2);
-        assertEquals(2D, invertResult, "Consumer should give expected output 2 as the multiplicative inverse of .5.");
-    }
-
-    @Test
-    void exceptionApplyTest1() {
-        CatchingConsumer<String> consumer = new CatchingConsumer<>(this::firstChar);
-        assertThrows(StringIndexOutOfBoundsException.class, () ->
-            consumer.accept(""), "Consumer should throw an exception if the arguments do not meet the preset conditions.");
-    }
-
-    @Test
-    void exceptionApplyTest2() {
-        CatchingConsumer<Double> consumer = new CatchingConsumer<>(this::inverse);
+    void exceptionApplyTest() {
+        CatchingConsumer<Integer> consumer = new CatchingConsumer<>(this::value);
         assertThrows(IllegalArgumentException.class, () ->
-            consumer.accept(0D), "Consumer should throw an exception if the arguments do not meet the preset conditions.");
+            consumer.accept(-1), "Consumer should throw an exception if the arguments do not meet the conditions.");
     }
 
-    void firstChar(String s) {
-        firstCharResult = s.charAt(0);
-    }
-
-    void inverse(double a) {
-        if (a == 0)
+    private int value(int a) {
+        if (a <= 0)
             throw new IllegalArgumentException();
-        invertResult = 1 / a;
+        return a;
     }
 }
