aaa3b44e5009ed7b4ec943bd97eec928a046845f
gabl <gabl22ytmc@gmail.com>
2022-06-29 16:58:37 +0200

    Edited JUnit Assertion messages

--- src/test/java/de/natrox/common/consumer/CatchingConsumerTest.java
+++ src/test/java/de/natrox/common/consumer/CatchingConsumerTest.java
@@ -14,16 +14,16 @@ class CatchingConsumerTest {
         AtomicInteger indicator = new AtomicInteger();
         CatchingConsumer<Integer> consumer = new CatchingConsumer<>((a) -> indicator.set(value(a)));
         consumer.accept(1);
-        assertEquals(1, indicator.get(), "Consumer should have performed actions correctly.");
+        assertEquals(1, indicator.get(), "Indicator should indicate the input of 1.");
         consumer.accept(2);
-        assertEquals(2, indicator.get(), "Consumer should have performed actions correctly.");
+        assertEquals(2, indicator.get(), "Indicator should indicate the input of 2.");
     }
 
     @Test
     void exceptionApplyTest() {
         CatchingConsumer<Integer> consumer = new CatchingConsumer<>(this::value);
         assertThrows(IllegalArgumentException.class, () ->
-            consumer.accept(-1), "Consumer should throw an exception if the arguments do not meet the conditions.");
+            consumer.accept(-1), "Consumer should throw an exception if the arguments don't meet the conditions.");
     }
 
     private int value(int a) {
