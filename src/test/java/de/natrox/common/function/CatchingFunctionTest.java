c22951544c36d1a98a168b21dbf82474da5d85a4
gabl <gabl22ytmc@gmail.com>
2022-06-29 19:35:14 +0200

    Rewrote CatchingFunctionTest

--- src/test/java/de/natrox/common/function/CatchingFunctionTest.java
+++ src/test/java/de/natrox/common/function/CatchingFunctionTest.java
@@ -2,45 +2,28 @@ package de.natrox.common.function;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CatchingFunctionTest {
 
     @Test
-    void defaultApplyTest1() {
-        CatchingFunction<String, Character> function = new CatchingFunction<>(this::firstChar);
-        assertEquals('f', function.apply("foo"), "Function should return 'f' as the first char of \"foo\".");
-        assertEquals('b', function.apply("boo"), "Function should return 'b' as the first char of \"boo\".");
+    void defaultApplyTest() {
+        CatchingFunction<Integer, Integer> function = new CatchingFunction<>(this::value);
+        assertEquals(1, function.apply(1), "Function should return the input of 1.");
+        assertEquals(2, function.apply(2), "Function should return the input of 2.");
     }
 
     @Test
-    void defaultApplyTest2() {
-        CatchingFunction<Double, Double> function = new CatchingFunction<>(this::inverse);
-        assertEquals(1D / 2, function.apply(2D), "Function should return .5 as the multiplicative inverse of 2.");
-        assertEquals(2D, function.apply(1D / 2), "Function should return 2 as the multiplicative inverse of .5.");
-    }
-
-    @Test
-    void exceptionApplyTest1() {
-        CatchingFunction<String, Character> function = new CatchingFunction<>(this::firstChar);
-        assertThrows(StringIndexOutOfBoundsException.class, () ->
-            function.apply(""), "Function should throw an exception if the arguments do not meet the preset conditions.");
-    }
-
-    @Test
-    void exceptionApplyTest2() {
-        CatchingFunction<Double, Double> function = new CatchingFunction<>(this::inverse);
+    void exceptionApplyTest() {
+        CatchingFunction<Integer, Integer> function = new CatchingFunction<>(this::value);
         assertThrows(IllegalArgumentException.class, () ->
-            function.apply(0D), "Function should throw an exception if the arguments do not meet the preset conditions.");
+            function.apply(-1), "Function should throw an exception if the arguments don't meet the conditions.");
     }
 
-    char firstChar(String s) {
-        return s.charAt(0);
-    }
-
-    double inverse(double a) {
-        if (a == 0)
+    private int value(int a) {
+        if (a <= 0)
             throw new IllegalArgumentException();
-        return 1 / a;
+        return a;
     }
 }
