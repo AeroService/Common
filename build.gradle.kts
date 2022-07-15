ed086f7e840eb0cf56225b4c287e8f962b23f503
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-07-15 02:12:05 +0200

    Update common dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,11 +29,12 @@ allprojects {
 
     repositories {
         mavenCentral()
+        maven("https://repo.natrox.de/repository/maven-public/")
     }
 }
 
 dependencies {
-    implementation(files("D:\\workspace\\NatroxMC\\Common\\build\\libs\\common.jar"))
+    implementation("de.natrox:Common:72c9211")
 
     implementation("org.jetbrains:annotations:23.0.0")
     implementation("org.slf4j:slf4j-api:2.0.0-alpha7")
